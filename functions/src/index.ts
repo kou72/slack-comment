b55e6badf5c8be18bf512196c2f5e8c26d60374b
kou72 <genpokopokopoko@gmail.com>
2021-09-20 15:23:01 +0900

    lint prettierの設定

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
 import * as functions from "firebase-functions";
+import admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  const writeResult = await admin
+    .firestore()
+    .collection("messages")
+    .add({ original: original });
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    const original = snap.data().original;
+    functions.logger.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
